0174362302add57b28d153bf4c3508501c7d5dbc
Geniye1 <jstrayer57@gmail.com>
2023-10-05 21:30:16 -0500

    Game now fully playable in browser

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function Gameboard() {
         currentBoard[i] = Cell();
     }
 
+    const _displayMove = (cell, marker) => {
+        cell.children[0].textContent = marker;
+    }
+
     const getIndicies = (marker) => {
         let indicies = [];
         currentBoard.forEach((cell, index) => {
@@ -15,11 +19,22 @@ function Gameboard() {
         return indicies;
     }
 
-    const applyMove = (cellNum, currentPlayer) => {
-        currentBoard[cellNum - 1].setValue(currentPlayer.marker);
+    const applyMove = (cell, cellNum, currentPlayer) => {
+        currentBoard[cellNum].setValue(currentPlayer.marker);
+        _displayMove(cell, currentPlayer.marker);
     }
 
-    return { applyMove, currentBoard, getIndicies }
+    const resetBoard = () => {
+        currentBoard.forEach((cell) => {
+            cell.setValue("");
+        });
+
+        gameCells.forEach((cell) => {
+            cell.children[0].textContent = "";
+        });
+    }
+
+    return { applyMove, currentBoard, getIndicies, resetBoard }
 }
 
 function Cell() {
@@ -48,6 +63,33 @@ const Game = (() => {
         [2, 4, 6]
     ]
 
+    const infoHeading = document.querySelector("#game-info-heading");
+    const date = new Date();
+    const welcomeMessage = "Welcome! Click a cell to begin playing.";
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // USED PURELY FOR THE 5th QUOTE
+    const moveQuotes = [
+        "That's uhh... haha, that's an interesting move for sure.",
+        "Great move, I bet your opponent has absolutely soiled themselves by now.",
+        "That's your move? You do know you have to play to win right?",
+        "WHOA. Absolutely INSANE move. Never would have seen that coming",
+        `You know, I envy you: playing Tic Tac Toe on a ${weekdays[date.getDate()]}. I'm just a computer, I will never know the touch of a human.`,
+        "Guys I hate to admit this since I'm literally the computer controlling this but this game is boring as fuck.",
+        "Another fantastic move, you could be world champion material you know (im being totally sarcastic).",
+        "Guys I got places to be can we hurry this up?"
+    ]
+    const winQuotes = [
+        "I bet you feel really satisfied right now. Relish this moment. It WILL be fleeting. Play again?",
+        "I hope this doesn't change your friendship, I know it changed mine... play again?",
+        "Incredible... simply incredible. I've never seen someone manage to accidentally win a game of Tic Tac Toe before. Astounding. Play again?",
+        "Yaaaaaaaaaaaaaayyyyy, god I wish I was put into a AI sex toy and not a Tic Tac Toe rip off. Play Again?",
+        "YES YES YES!!! YEEESS!!! FUCK YESSSS!!!! IM SO FUCKING HAPPY RIGHT NOWWWWWW!!!! PLAY AGAIN??? OF COURSE YOU DO!!!"
+    ]
+    const tieQuotes = [
+        "I hate to be the bearer of bad news, but you guys fucking suck at this game. Try again?",
+        "hAHAHAHAHAHAHAH YOU TIED??!?!?! HAHAHAHAHAAHAHHAHAHA LMAO LMAO LMAO LMAO LMAO TRY AGAIN PLEASE",
+        "Oooooo... yikes. Looks like you guys tied. I am so sorry (im not im being sarcastic again). Try again?"
+    ]
+
     const board = Gameboard();
 
     const players = [
@@ -62,9 +104,12 @@ const Game = (() => {
     ]
     let currentPlayer = players[0];
 
-    const _printInitialMessage = () => {
-        console.log("Welcome! You are 'X', type call playRound on Game with your desired x and y position to play!");
-    }
+    // If this becomes true at all then the game has ended and no more moves should be allowed
+    let hasGameEnded = false;
+
+    // const _printInitialMessage = () => {
+    //     console.log("Welcome! You are 'X', type call playRound on Game with your desired x and y position to play!");
+    // }
 
     const _switchPlayerTurn = () => {
         currentPlayer = ((currentPlayer == players[0]) ? players[1] : players[0]);
@@ -83,31 +128,56 @@ const Game = (() => {
         });
 
         if (!hasWon) {
-            return;
+            // If they haven't won AND there are 4 indicies in the array, then that means a tie has been reached as that is the max moves any player can make
+            if (playerMoveIndicies.length === 5) {
+                infoHeading.textContent = tieQuotes[Math.floor(Math.random() * tieQuotes.length)];
+                hasGameEnded = true;
+                return;
+            } 
+            // Otherwise, no tie has been reached and we can continue the game
+            else {
+                infoHeading.textContent = moveQuotes[Math.floor(Math.random() * moveQuotes.length)];
+                return;
+            }
         }
 
-        console.log("Holy fuck you did it oh my god thats so awesome wow great job (im being sarcastic)");
+        // If they have won, display win message
+        infoHeading.textContent = winQuotes[Math.floor(Math.random() * winQuotes.length)];
+        hasGameEnded = true;
     }
 
-    const playRound = (cellNum) => {
-        board.applyMove(cellNum, currentPlayer);
-        _checkForWin();
-        _switchPlayerTurn();
+    const initializeGame = () => {
+        infoHeading.textContent = welcomeMessage;
     }
 
-    _printInitialMessage();
+    const playRound = (cell, cellId) => {
+        if (!hasGameEnded) {
+            board.applyMove(cell, cellId, currentPlayer);
+            _checkForWin();
+            _switchPlayerTurn();
+        }
+    }
 
-    return { playRound, board }
+    const reset = () => {
+        board.resetBoard();
+        initializeGame();
+        hasGameEnded = false;
+    }
+
+    initializeGame();
+
+    return { playRound, board, reset }
 })();
 
-/*
-    Game creates Gameboard object
-    In each position of internal board array, create a new Cell
-    Each cell must hold its current value, ('', 'X', or 'O'), and two methods to get and set its value
-    Gameboard object initializes board
-    Game prints initial message
-    player does Game.playRound(cellNum)
-    Game passes cellNum and the currentPlayer to the Gameboard() object with applyMove() for processing
-    Game switches player turn
-    Gameboard updates value of cell at position cellNum - 1 in array (since the player will be going from 1 - 9)
-*/
+const gameCells = document.querySelectorAll(".game-cell");
+gameCells.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+        console.log(e.target);
+        Game.playRound(e.target, e.target.dataset.id);
+    })
+})
+
+const resetButton = document.querySelector("svg");
+resetButton.addEventListener("click", () => {
+    Game.reset();
+})
